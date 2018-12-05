70879b5d6d8dc804207b5ff2cd140250e83721c5
k123321141 <kk123321141@gamil.com>
2018-12-05 22:34:41 +0800

    finish hw3-1 report

--- hw3/task1/predict.sh
+++ hw3/task1/predict.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 mkdir data
-#wget https://www.dropbox.com/s/4c536h3nreb09wc/r1-r2.txt?dl=0 -O data/r1-r2.txt
-#wget https://www.dropbox.com/s/lpza12vae7u16oz/r3-r4.txt?dl=0 -O data/r3-r4.txt
+wget https://www.dropbox.com/s/4c536h3nreb09wc/r1-r2.txt?dl=0 -O data/r1-r2.txt
+wget https://www.dropbox.com/s/lpza12vae7u16oz/r3-r4.txt?dl=0 -O data/r3-r4.txt
 mkdir src
 mkdir src/models
-# wget https://www.dropbox.com/s/sgds892rbdwtyl3/decoder-att.pl?dl=0 -O src/models/decoder-att.pl
-# wget https://www.dropbox.com/s/121fc18g1vs6r95/encoder-att.pl?dl=0 -O src/models/encoder-att.pl
-#wget https://www.dropbox.com/s/ojd6cg1z0q77bkr/decoder-rnn.pl?dl=0 -O src/models/decoder-rnn.pl
-#wget https://www.dropbox.com/s/1ga27rrkkmwazbn/encoder-rnn.pl?dl=0 -O src/models/encoder-rnn.pl
+wget https://www.dropbox.com/s/sgds892rbdwtyl3/decoder-att.pl?dl=0 -O src/models/decoder-att.pl
+wget https://www.dropbox.com/s/121fc18g1vs6r95/encoder-att.pl?dl=0 -O src/models/encoder-att.pl
+wget https://www.dropbox.com/s/ojd6cg1z0q77bkr/decoder-rnn.pl?dl=0 -O src/models/decoder-rnn.pl
+wget https://www.dropbox.com/s/1ga27rrkkmwazbn/encoder-rnn.pl?dl=0 -O src/models/encoder-rnn.pl
 input=$1
 output=$2
 python2 src/predict_rnn.py $1 $2
---
+++ hw3/task1/r06922115/src/predict_rnn.py
@@ -0,0 +1,250 @@
+import argparse
+import re
+import sys
+import unicodedata
+import string
+import random
+import time
+import math
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torch import optim
+from tqdm import tqdm
+import numpy as np
+import torch.nn.functional as F
+reload(sys)
+sys.setdefaultencoding('utf-8')
+USE_CUDA = True
+SOS_token = 0
+EOS_token = 1
+
+class Lang:
+    def __init__(self, name):
+        self.name = name
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {0: "SOS", 1: "EOS"}
+        self.n_words = 2 # Count SOS and EOS
+    def index_words(self, sentence):
+        for word in sentence.split(' '):
+            self.index_word(word)
+
+    def index_word(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.n_words
+            self.word2count[word] = 1
+            self.index2word[self.n_words] = word
+            self.n_words += 1
+        else:
+            self.word2count[word] += 1
+def normalize_string(s):
+#     s = unicode_to_ascii(s.lower().strip())
+    s = s.strip()
+#     print s,'start'
+    s = re.sub(u"([.!?])", u" \1", s)
+#     s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+    s = re.sub(u"[^\u4e00-\u9fffa-zA-Z.!?0-9]+", r" ", s)
+#     print s,'end'
+    return s
+def read_langs(lang1, lang2, reverse=False):
+    print("Reading lines...")
+
+    # Read the file and split into lines
+    lines = open('./data/%s-%s.txt' % (lang1, lang2)).read().strip().split('\n')
+    
+    # Split every line into pairs and normalize
+    pairs = [[normalize_string(s.decode('utf8')) for s in l.split('\t')] for l in lines]
+    
+    # Reverse pairs, make Lang instances
+    if reverse:
+        pairs = [list(reversed(p)) for p in pairs]
+        input_lang = Lang(lang2)
+        output_lang = Lang(lang1)
+    else:
+        input_lang = Lang(lang1)
+        output_lang = Lang(lang2)
+        
+    return input_lang, output_lang, pairs
+MAX_LENGTH = 60
+
+good_prefixes = (
+    "i am ", "i m ",
+    "he is", "he s ",
+    "she is", "she s",
+    "you are", "you re "
+)
+
+def filter_pair(p):
+#     return len(p[0].split(' ')) < MAX_LENGTH and len(p[1].split(' ')) < MAX_LENGTH and \
+#         p[1].startswith(good_prefixes)
+    return len(p[0].split(' ')) < MAX_LENGTH and len(p[1].split(' ')) < MAX_LENGTH
+
+def filter_pairs(pairs):
+    return [pair for pair in pairs if filter_pair(pair)]
+def prepare_data(lang1_name, lang2_name, reverse=False):
+    input_lang, output_lang, pairs = read_langs(lang1_name, lang2_name, reverse)
+    print("Read %s sentence pairs" % len(pairs))
+    
+    pairs = filter_pairs(pairs)
+    print("Trimmed to %s sentence pairs" % len(pairs))
+    
+    print("Indexing words...")
+    for pair in pairs:
+        input_lang.index_words(pair[0])
+        output_lang.index_words(pair[1])
+
+    return input_lang, output_lang, pairs
+
+# input_lang, output_lang, pairs = prepare_data('eng', 'fra', True)
+# input_lang, output_lang, pairs = prepare_data('eng', 'cmn', True)
+input_lang, output_lang, pairs = prepare_data('r3', 'r4', True)
+# Print an example pair
+# Return a list of indexes, one for each word in the sentence
+def indexes_from_sentence(lang, sentence):
+    return [lang.word2index[word] for word in sentence.split(' ')]
+
+def variable_from_sentence(lang, sentence):
+    indexes = indexes_from_sentence(lang, sentence)
+    indexes.append(EOS_token)
+    var = Variable(torch.LongTensor(indexes).view(-1, 1))
+#     print('var =', var)
+    if USE_CUDA: var = var.cuda()
+    return var
+
+def variables_from_pair(pair):
+    input_variable = variable_from_sentence(input_lang, pair[0])
+    target_variable = variable_from_sentence(output_lang, pair[1])
+    return (input_variable, target_variable)
+class EncoderRNN(nn.Module):
+    def __init__(self, input_size, hidden_size, n_layers=1):
+        super(EncoderRNN, self).__init__()
+        
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.n_layers = n_layers
+        
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size, n_layers)
+        
+    def forward(self, word_inputs, hidden):
+        # Note: we run this all at once (over the whole input sequence)
+        seq_len = len(word_inputs)
+        embedded = self.embedding(word_inputs).view(seq_len, 1, -1)
+        output, hidden = self.gru(embedded, hidden)
+        return output, hidden
+
+    def init_hidden(self):
+        hidden = Variable(torch.zeros(self.n_layers, 1, self.hidden_size))
+        if USE_CUDA: hidden = hidden.cuda()
+        return hidden
+class DecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size, n_layers=1, dropout_p=0.1):
+        super(DecoderRNN, self).__init__()
+        
+        # Keep parameters for reference
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.n_layers = n_layers
+        self.dropout_p = dropout_p
+        
+        # Define layers
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size, n_layers, dropout=dropout_p)
+        self.out = nn.Linear(hidden_size, output_size)
+        
+    
+    def forward(self, word_input, last_hidden, encoder_outputs):
+        # Note: we run this one step at a time
+        
+        # Get the embedding of the current input word (last output word)
+        word_embedded = self.embedding(word_input).view(1, 1, -1) # S=1 x B x N
+        
+        # Combine embedded input word and last context, run through RNN
+        rnn_input = word_embedded
+        rnn_output, hidden = self.gru(rnn_input, last_hidden)
+
+        
+        # Final output layer (next word prediction) using the RNN hidden state and context vector
+        rnn_output = rnn_output.squeeze(0) # S=1 x B x N -> B x N
+        output = F.log_softmax(self.out(rnn_output), dim=-1)
+        
+        # Return final output, hidden state, and attention weights (for visualization)
+        return output, hidden
+def evaluate(sentence, max_length=MAX_LENGTH):
+    input_variable = variable_from_sentence(input_lang, sentence)
+    input_length = input_variable.size()[0]
+    
+    # Run through encoder
+    encoder_hidden = encoder.init_hidden()
+    encoder_outputs, encoder_hidden = encoder(input_variable, encoder_hidden)
+
+    # Create starting vectors for decoder
+    decoder_input = Variable(torch.LongTensor([[SOS_token]])) # SOS
+    if USE_CUDA:
+        decoder_input = decoder_input.cuda()
+
+    decoder_hidden = encoder_hidden
+    
+    decoded_words = []
+    
+    # Run through decoder
+    for di in range(max_length):
+        decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden, encoder_outputs)
+        # Choose top word from output
+        topv, topi = decoder_output.data.topk(1)
+        ni = topi[0][0]
+        if ni == EOS_token:
+            decoded_words.append('<EOS>')
+            break
+        else:
+            decoded_words.append(output_lang.index2word[ni.item()])
+            
+        # Next input is chosen word
+        decoder_input = Variable(torch.LongTensor([[ni]]))
+        if USE_CUDA: decoder_input = decoder_input.cuda()
+    return decoded_words
+    
+def testing(sentence):
+    sentence = sentence.replace('SOS ','').replace('EOS ','').strip()
+    while True:
+        try:
+            output_words = evaluate(sentence)
+            break
+        except KeyError,e:
+            sub = input_lang.index2word[np.random.randint(input_lang.n_words)]
+            sentence = sentence.replace(e.message,sub)
+
+    s = ''
+    for y in output_words:
+        s += y + ' '
+    return s.replace('<EOS>','').replace('SOS','').replace('EOS','').strip()
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser(description="Testing")
+    parser.add_argument('input', type=str)
+    parser.add_argument('output', type=str)
+    args = parser.parse_args()
+    print args
+    encoder = torch.load('./src/models/encoder-rnn.pl')
+    decoder = torch.load('./src/models/decoder-rnn.pl')
+    encoder.eval()
+    decoder.eval()
+    
+
+    path = args.input
+    out_path = args.output
+    with open(path,'r') as f:
+        ls = f.readlines()
+
+    with open(out_path,'w') as f:
+        with torch.no_grad():
+            with tqdm(total=len(ls)) as pbar:
+                for l in ls:
+                    l = l.decode('utf8').strip()
+                    s = testing(l)+'\n'
+                    f.write(s.encode('utf8'))
+                    pbar.update(1)
+    print 'done'
+    
